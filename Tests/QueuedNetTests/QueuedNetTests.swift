0271c0739f3ec79b8afd67a83aa8500b687e2fe6
Valentin Knabel <develop@vknabel.com>
2016-09-26 20:34:24 +0200

    Added Linux tests support

--- Tests/QueuedNetTests/QueuedNetTests.swift
+++ Tests/QueuedNetTests/QueuedNetTests.swift
@@ -8,28 +8,22 @@
 
 import XCTest
 
-class QueuedNetTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+protocol LinuxTestCase {
+    associatedtype TestCase: XCTestCase
+    static var allTests: [(String, (TestCase) -> () -> ())] { get }
+}
+
+#if os(Linux)
+    public func allTests() -> [XCTestCaseEntry] {
+        return [
+            testCase(QueuedNetTests.allTests)
+        ]
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
+#endif
+
+
+class QueuedNetTests: XCTestCase, LinuxTestCase {
+
+    static var allTests = []
     
 }
